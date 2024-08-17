c93152ef10b79e4e57a68b41bffa6f6aade834da
JangRuBin2 <wkdfnqls2465@gmail.com>
2024-08-17 14:58:16 +0900

    체력 0되면 대사 변경, 다음 토마토로 바꿔야 함

--- pingpong/app/tomato/TomatoPage.tsx
+++ pingpong/app/tomato/TomatoPage.tsx
@@ -1,8 +1,10 @@
 "use client";
+import textBox from "@/public/images/textBox.png";
 import tomato from "@/public/images/tomato.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import styles from "./TomatoPage.module.css";
 
 const TomatoPage = () => {
   const [tomatoValue, setTomatoValue] = useState({
@@ -11,41 +13,60 @@ const TomatoPage = () => {
     sex: "tomato",
     job: "goodBoy",
   });
+
+  const [tomatoState, setTomatoState] = useState({
+    name: "토마토 빌런",
+    HP: 30,
+    comment: "나는 토마토 악당! 널 죽인다!",
+  });
+
   const [isShaking, setIsShaking] = useState(false);
+  const router = useRouter();
+
   const handleShake = () => {
     setIsShaking(true);
-    setTimeout(() => setIsShaking(false), 500); // 애니메이션 종료 후 상태 리셋
-    setTomatoHP((prev) => prev - 1);
+    setTimeout(() => setIsShaking(false), 500);
+
+    setTomatoState((prev) => ({
+      ...prev,
+      HP: prev.HP - 1,
+    }));
   };
-  const [tomatoHP, setTomatoHP] = useState<number>(30);
-  const router = useRouter();
+
+  useEffect(() => {
+    if (tomatoState.HP === 0) {
+      setTomatoState((prev) => ({
+        ...prev,
+        comment: "날... 쓰러뜨리다니... 대단...하다...",
+      }));
+    }
+  }, [tomatoState.HP]);
+
   const handleTomage = () => {
-    setTomatoValue((prev) => ({ ...prev, name: "김토마토", age: 13 }));
+    setTomatoValue((prev) => ({
+      ...prev,
+      name: "김토마토",
+      age: 13,
+    }));
   };
+
   const backToMain = () => {
     router.push("/");
   };
-  useEffect(() => {
-    console.log("tomatoValue : ", tomatoValue);
-    console.log("tomatoHP : ", tomatoHP);
-  }, [tomatoValue, tomatoHP]);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100vw",
-        height: "100vh",
-        border: "1px solid black",
-        justifyContent: "space-between",
-      }}
-    >
-      <h2>{"토마토에욤"}</h2>
+    <div className={styles.container}>
+      <h2>{tomatoState.name}</h2>
+      <div className={styles.imageWrapper}>
+        <Image src={textBox} alt="Text Box" width={100} height={100} />
+        <p className={styles.comment}>{tomatoState.comment}</p>
+      </div>
       <Image
         src={tomato}
         alt="Tomato Image"
         width={300}
         height={300}
-        className={isShaking ? "shake" : ""}
+        className={isShaking ? styles.shake : ""}
       />
       <button onClick={handleShake}>{"토마토 공격!!"}</button>
       <div style={{ height: "30px" }}>
