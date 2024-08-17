021136502e018ca11c80dd9ef38017421475417c
JangRuBin2 <wkdfnqls2465@gmail.com>
2024-08-17 13:25:53 +0900

    토마토!

--- pingpong/app/page.tsx
+++ pingpong/app/page.tsx
@@ -1,5 +1,12 @@
 // import styles from "./page.module.css";
 
+import Link from "next/link";
+
 export default function Home() {
-  return <div>hello world!</div>;
+  return (
+    <div>
+      <h2>{"hello world!"}</h2>
+      <Link href="/tomato">{"Move to '/tomato'"}</Link>
+    </div>
+  );
 }
---
+++ pingpong/app/tomato/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>{"토마토!"}</div>;
+};
+
+export default page;
