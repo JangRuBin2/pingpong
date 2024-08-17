431b1b6bc236d8c4d89baf621ae252587abb9c72
JangRuBin2 <wkdfnqls2465@gmail.com>
2024-08-17 15:18:12 +0900

    스테이지, 토마토 state분리. css 적용 전

--- pingpong/app/tomato/TomatoPage.tsx
+++ pingpong/app/tomato/TomatoPage.tsx
@@ -1,27 +1,38 @@
 "use client";
 import textBox from "@/public/images/textBox.png";
-import tomato from "@/public/images/tomato.png";
+import tomatoImg from "@/public/images/tomato.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { tomato, Tomato } from "../tomatos/tomatos";
 import styles from "./TomatoPage.module.css";
 
 const TomatoPage = () => {
+  const router = useRouter();
+  // 토마토 커스텀 기능. 일단 보류
   const [tomatoValue, setTomatoValue] = useState({
     name: "Kimato",
     age: 12,
     sex: "tomato",
     job: "goodBoy",
   });
+  const handleTomage = () => {
+    setTomatoValue((prev) => ({
+      ...prev,
+      name: "김토마토",
+      age: 13,
+    }));
+  };
 
-  const [tomatoState, setTomatoState] = useState({
-    name: "토마토 빌런",
-    HP: 30,
-    comment: "나는 토마토 악당! 널 죽인다!",
+  // 토마토 상태관리
+  const [tomatoState, setTomatoState] = useState<Tomato>(tomato);
+  // 스테이지 상태관리
+  const [stageStatus, setStageStatus] = useState({
+    stage: 1,
+    status: false,
   });
-
+  // 토마토 쳐맞을 때 css 효과
   const [isShaking, setIsShaking] = useState(false);
-  const router = useRouter();
 
   const handleShake = () => {
     setIsShaking(true);
@@ -33,45 +44,53 @@ const TomatoPage = () => {
     }));
   };
 
+  // 토마토 체력 변화 생길 때 스테이지 관리
   useEffect(() => {
     if (tomatoState.HP === 0) {
-      setTomatoState((prev) => ({
+      // 상태 추가할 수도 있어서 ...prev
+      setStageStatus((prev) => ({
         ...prev,
-        comment: "날... 쓰러뜨리다니... 대단...하다...",
+        stage: prev.stage++,
+        status: true,
       }));
     }
   }, [tomatoState.HP]);
 
-  const handleTomage = () => {
-    setTomatoValue((prev) => ({
-      ...prev,
-      name: "김토마토",
-      age: 13,
-    }));
-  };
-
   const backToMain = () => {
     router.push("/");
   };
-
+  // 스테이지 핸들링
+  const handleStage = () => {
+    setStageStatus((prev) => ({ ...prev, stage: prev.stage++ }));
+  };
   return (
     <div className={styles.container}>
       <h2>{tomatoState.name}</h2>
       <div className={styles.imageWrapper}>
         <Image src={textBox} alt="Text Box" width={100} height={100} />
-        <p className={styles.comment}>{tomatoState.comment}</p>
+        {/* 토마토 체력에 따라서 코멘트 변경 */}
+        {tomatoState.HP === 0 ? (
+          <p className={styles.comment}>{tomatoState.lastComment}</p>
+        ) : (
+          <p className={styles.comment}>{tomatoState.comment}</p>
+        )}
       </div>
       <Image
-        src={tomato}
+        src={tomatoImg}
         alt="Tomato Image"
         width={300}
         height={300}
         className={isShaking ? styles.shake : ""}
       />
-      <button onClick={handleShake}>{"토마토 공격!!"}</button>
+      {stageStatus.status ? (
+        <button onClick={handleStage}>{"다음 토마토로 이동"}</button>
+      ) : (
+        <button onClick={handleShake}>{"토마토 공격!!"}</button>
+      )}
+
       <div style={{ height: "30px" }}>
-        <button onClick={handleTomage}>{"토마토 핸들링"}</button>
-        <button onClick={backToMain}>{"메인페이지로 돌아갓"}</button>
+        <button onClick={handleTomage}>{"토마토 꾸미기"}</button>
+        <button onClick={backToMain}>{"메인페이지로"}</button>
       </div>
     </div>
   );
---
+++ pingpong/app/tomatos/tomatos.ts
@@ -0,0 +1,12 @@
+export type Tomato = {
+  name: string;
+  HP: number;
+  comment: string;
+  lastComment: string;
+};
+export const tomato: Tomato = {
+  name: "토마토 빌런",
+  HP: 30,
+  comment: "나는 토마토 악당! 널 죽인다!",
+  lastComment: "대단...하군...",
+};
