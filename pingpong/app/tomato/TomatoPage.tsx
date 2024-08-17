0a9d1c6ef4227952e93684d5fada961f377a2cad
JangRuBin2 <wkdfnqls2465@gmail.com>
2024-08-17 14:34:23 +0900

    토마토 공격하기

--- pingpong/app/tomato/TomatoPage.tsx
+++ pingpong/app/tomato/TomatoPage.tsx
@@ -1,4 +1,6 @@
 "use client";
+import tomato from "@/public/images/tomato.png";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -9,6 +11,11 @@ const TomatoPage = () => {
     sex: "tomato",
     job: "goodBoy",
   });
+  const [isShaking, setIsShaking] = useState(false);
+  const handleShake = () => {
+    setIsShaking(true);
+    setTimeout(() => setIsShaking(false), 500); // 애니메이션 종료 후 상태 리셋
+  };
   const router = useRouter();
   const handleTomage = () => {
     setTomatoValue((prev) => ({ ...prev, name: "김토마토", age: 13 }));
@@ -23,13 +30,21 @@ const TomatoPage = () => {
     <div
       style={{
         display: "flex",
-        width: "500px",
-        height: "400px",
+        width: "100vw",
+        height: "100vh",
         border: "1px solid black",
         justifyContent: "space-between",
       }}
     >
       <h2>{"토마토에욤"}</h2>
+      <Image
+        src={tomato}
+        alt="Tomato Image"
+        width={300}
+        height={300}
+        className={isShaking ? "shake" : ""}
+      />
+      <button onClick={handleShake}>{"토마토 공격!!"}</button>
       <div style={{ height: "30px" }}>
         <button onClick={handleTomage}>{"토마토 핸들링"}</button>
         <button onClick={backToMain}>{"메인페이지로 돌아갓"}</button>
