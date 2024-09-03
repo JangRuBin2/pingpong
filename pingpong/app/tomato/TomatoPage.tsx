96f12e6e2ea156d327df1567b6f70f73c76c49f3
JangRuBin2 <wkdfnqls2465@gmail.com>
2024-09-03 22:33:56 +0900

    빌런 더미데이터 배열로생성, 상태관리 추가

--- pingpong/app/tomato/TomatoPage.tsx
+++ pingpong/app/tomato/TomatoPage.tsx
@@ -1,82 +1,94 @@
 "use client";
 import textBox from "@/public/images/textBox.png";
 import tomatoImg from "@/public/images/tomato.png";
+
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { tomato, Tomato } from "../tomatos/tomatos";
+import { Villain, villans } from "../tomatos/tomatos";
 import styles from "./TomatoPage.module.css";
 
 const TomatoPage = () => {
   const router = useRouter();
   // 토마토 커스텀 기능. 일단 보류
-  const [tomatoValue, setTomatoValue] = useState({
-    name: "Kimato",
-    age: 12,
-    sex: "tomato",
-    job: "goodBoy",
-  });
-  const handleTomage = () => {
-    setTomatoValue((prev) => ({
-      ...prev,
-      name: "김토마토",
-      age: 13,
-    }));
-  };
+  // const [tomatoValue, setTomatoValue] = useState({
+  //   name: "Kimato",
+  //   age: 12,
+  //   sex: "tomato",
+  //   job: "goodBoy",
+  // });
+  // const handleTomage = () => {
+  //   setTomatoValue((prev) => ({
+  //     ...prev,
+  //     name: "김토마토",
+  //     age: 13,
+  //   }));
+  // };
 
-  // 토마토 상태관리
-  const [tomatoState, setTomatoState] = useState<Tomato>(tomato);
+  // 보스 몬스터 상태관리
+  const [villanState, setVillanState] = useState<Villain[]>(villans);
   // 스테이지 상태관리
   const [stageStatus, setStageStatus] = useState({
-    stage: 1,
+    stage: 0,
     status: false,
   });
-  // 토마토 쳐맞을 때 css 효과
+  // 토마토가 쳐맞을 때 css 효과
   const [isShaking, setIsShaking] = useState(false);
+  // 사용자 스탯
+  const [userInfo, setUserInfo] = useState({
+    level: 1,
+    damage: 1,
+  });
+  const nowStage = stageStatus.stage;
 
-  const handleShake = () => {
+  const attackVillan = (damage: number) => {
     setIsShaking(true);
     setTimeout(() => setIsShaking(false), 500);
+    setVillanState((prevVillains) => {
+      const currentVillain = prevVillains[nowStage];
 
-    setTomatoState((prev) => ({
-      ...prev,
-      HP: prev.HP - 1,
-    }));
+      const updatedVillain = {
+        ...currentVillain,
+        HP: currentVillain.HP != 0 ? currentVillain.HP - damage : damage,
+      };
+      const newVillains = [...prevVillains];
+      newVillains[stageStatus.stage] = updatedVillain;
+
+      return newVillains;
+    });
   };
 
   // 토마토 체력 변화 생길 때 스테이지 관리
   useEffect(() => {
-    if (tomatoState.HP === 0) {
-      // 상태 추가할 수도 있어서 ...prev
-      setStageStatus((prev) => ({
-        ...prev,
-        stage: prev.stage++,
+    if (villans[nowStage].HP === 0) {
+      setStageStatus((prevStage) => ({
+        ...prevStage,
+        stage: prevStage.stage++,
         status: true,
       }));
     }
-  }, [tomatoState.HP]);
+  }, [villanState]);
 
   const backToMain = () => {
     router.push("/");
   };
   // 스테이지 핸들링
   const handleStage = () => {
-    setStageStatus((prev) => ({ ...prev, stage: prev.stage++ }));
+    setStageStatus((prev) => ({ ...prev, stage: prev.stage++, ready: true }));
   };
   return (
     <div className={styles.container}>
-      <h2>{tomatoState.name}</h2>
+      <h2>{villanState[nowStage].name}</h2>
       <div className={styles.imageWrapper}>
         <Image src={textBox} alt="Text Box" width={100} height={100} />
-        {/* 토마토 체력에 따라서 코멘트 변경 */}
-        {tomatoState.HP === 0 ? (
-          <p className={styles.comment}>{tomatoState.lastComment}</p>
+        {villanState[nowStage]["HP"] === 0 ? (
+          <p className={styles.comment}>{villanState[nowStage].lastComment}</p>
         ) : (
-          <p className={styles.comment}>{tomatoState.comment}</p>
+          <p className={styles.comment}>{villanState[nowStage].comment}</p>
         )}
       </div>
       <Image
-        src={tomatoImg}
+        src={villanState[nowStage].img || tomatoImg}
         alt="Tomato Image"
         width={300}
         height={300}
@@ -85,11 +97,13 @@ const TomatoPage = () => {
       {stageStatus.status ? (
         <button onClick={handleStage}>{"다음 토마토로 이동"}</button>
       ) : (
-        <button onClick={handleShake}>{"토마토 공격!!"}</button>
+        <button onClick={() => attackVillan(userInfo.damage)}>
+          {"토마토 공격!!"}
+        </button>
       )}
 
       <div style={{ height: "30px" }}>
-        <button onClick={handleTomage}>{"토마토 꾸미기"}</button>
+        {/* <button onClick={handleTomage}>{"토마토 꾸미기"}</button> */}
         <button onClick={backToMain}>{"메인페이지로"}</button>
       </div>
     </div>
--- pingpong/app/tomatos/tomatos.ts
+++ pingpong/app/tomatos/tomatos.ts
@@ -1,12 +1,29 @@
-export type Tomato = {
+import carrotImg from "@/public/images/carrot.png";
+import tomatoImg from "@/public/images/tomato.png";
+import { StaticImageData } from "next/image";
+export type Villain = {
   name: string;
   HP: number;
   comment: string;
   lastComment: string;
+  img: StaticImageData | undefined;
+  stage: number;
 };
-export const tomato: Tomato = {
-  name: "토마토 빌런",
-  HP: 30,
-  comment: "나는 토마토 악당! 널 죽인다!",
-  lastComment: "대단...하군...",
-};
+export const villans: Villain[] = [
+  {
+    name: "토마토 빌런",
+    HP: 30,
+    comment: "나는 토마토 악당! 널 죽인다!",
+    lastComment: "대단...하군..",
+    img: tomatoImg,
+    stage: 1,
+  },
+  {
+    name: "당근백작",
+    HP: 60,
+    comment: "of course를 직역하면... 당근이지... 케켁",
+    lastComment: "꾸에에에엥",
+    img: carrotImg,
+    stage: 2,
+  },
+];
